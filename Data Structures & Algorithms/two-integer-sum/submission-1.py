051d8a0ce7342f11b4d7024bafa5e38eede218f0
Varshini Gopi <118385232+Varshini2005@users.noreply.github.com>
2026-07-17 21:41:02 +0530

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        mpp={}
+        for i in range(n):
+            diff = target - nums[i]
+            if diff in mpp:
+                return [mpp[diff],i]
+            mpp[nums[i]] = i
+
+
+
+        
